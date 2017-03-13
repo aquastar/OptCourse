ab02dc1d6dbb1356ab146413bc8306d962960ff5
aquastar <qqadd520@126.com>
2017-03-12 21:43:17 -0400

    update

--- interior.py
+++ interior.py
@@ -2,15 +2,14 @@
 # 1: (1,1) (2,2) (2,0) (2,1)
 # 2: (0,0) (1,0) (0,1) (-1,-1)
 
-import cvxopt, svmcmpl
+import svmcmpl
+import time
 from cvxopt import matrix
 
 X = matrix([1.0, 2.0, 2.0, 2.0, 0.0, 1.0, 0.0, -1.0, 1.0, 2.0, 0.0, 1.0, 0.0, 0.0, 1.0, -1.0], (8, 2))
 d = matrix([1, 1, 1, 1, -1, -1, -1, -1])
-print X, d
 gamma = 2.0
-kernel = 'rbf'
+kernel = 'linear'
 sigma = 1.0
-# width = 20
+
 sol1 = svmcmpl.softmargin(X, d, gamma, kernel, sigma)
-# sol2 = svmcmpl.softmargin_appr(X, d, gamma, width, kernel, sigma)
